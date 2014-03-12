b3845df3c3e2f65c7ed26de0af3fa12b363410b3
Atsushi OHNO <frah@tokcs.com>
2014-03-13 00:46:23 +0900

    Add WebSocket implementation.

--- pc.js
+++ pc.js
@@ -24,18 +24,33 @@ sock.onclose = function () {
 };
 sock.onmessage = function(ev) {
   console.log('WebSocket message received: ' + ev.data);
+  try {
+    var j = $.parseJSON(ev.data);
+    switch (j.type) {
+      case 1:
+        $('#ws_id').text(j.id);
+        break;
+      case 2:
+        $('#init_view').css('display', 'none');
+        initMap(j.lat, j.lng);
+        break;
+    }
+  } catch (e) {
+    console.log(e);
+    return;
+  }
 };
 
 /*
  Google maps
  */
-function initMap() {
+function initMap(lat, lng) {
   console.log('Initialize Google Maps API');
   var _mapCanvas = document.getElementById('map');
   _mapCanvas.style.height = document.documentElement.clientHeight + 'px';
   var _mapOptions = {
-    center: new google.maps.LatLng(38.26572, 136.23100),
-    zoom: 5,
+    center: new google.maps.LatLng(lat, lng),
+    zoom: 18,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(_mapCanvas, _mapOptions);
@@ -48,5 +63,3 @@ google.maps.event.addDomListener(window, "resize", function() {
   google.maps.event.trigger(map, "resize");
   map.setCenter(_center);
 });
-
-initMap();
--- smart.js
+++ smart.js
@@ -18,31 +18,57 @@ sock.onclose = function () {
 };
 sock.onmessage = function(ev) {
   console.log('WebSocket message received: ' + ev.data);
+  try {
+    var j = $.parseJSON(ev.data);
+    switch (j.type) {
+      case 1:
+        break;
+    }
+  } catch (e) {
+    console.log(e);
+    return;
+  }
 };
 
-if (navigator.geolocation) {
-  $('#latlng').text('positioning...');
-  var _geoOption = {
-    enableHighAccuracy: true,
-    timeout: 60000
-  };
-  navigator.geolocation.getCurrentPosition(
-    function(loc) {
-      var _latlng = loc.coords.latitude + ', ' + loc.coords.longitude;
-      $('#latlng').text(_latlng);
-    }, function(err) {
-      var _err_mes = '';
-      switch (err.code) {
-        case 1:
-          _err_mes = '位置情報の利用が許可されていません';
-          break;
-        case 2:
-          _err_mes = 'デバイスの位置が判定できません';
-          break;
-        case 3:
-          _err_mes = 'タイムアウトしました';
-          break;
-      }
-      $('#latlng').text(_err_mes);
-    }, _geoOption);
+/*
+ Geolocation
+ */
+function doGeolocate() {
+  if (navigator.geolocation) {
+    var _geoOption = {
+      enableHighAccuracy: true,
+      timeout: 60000
+    };
+    navigator.geolocation.getCurrentPosition(
+      function(loc) {
+        var d = {
+          type: 30,
+          id: $('#code_input').val(),
+          lat: loc.coords.latitude,
+          lng: loc.coords.longitude
+        };
+        sock.send(JSON.stringify(d));
+      }, function(err) {
+        var _err_mes = '';
+        switch (err.code) {
+          case 1:
+            _err_mes = '位置情報の利用が許可されていません';
+            break;
+          case 2:
+            _err_mes = 'デバイスの位置が判定できません';
+            break;
+          case 3:
+            _err_mes = 'タイムアウトしました';
+            break;
+        }
+        alert('現在地の取得に失敗しました\n' + _err_mes);
+        return;
+      }, _geoOption);
+  } else {
+    alert('現在地の取得ができません');
+  }
 }
+
+$('#code_send').click(function(){
+  doGeolocate();
+});
